94006c75bd2e0f49b502eeed267d1586af78f838
Hamaad0092 <hamaadsaeed9712@gmail.com>
2024-03-23 16:48:53 +0000

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
     projectId: "1f5byj",
   e2e: {
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
