dc70d69b45c760947544e9bc32cc6984ac1f6065
Hamaad0092 <hamaadsaeed9712@gmail.com>
2024-03-25 19:46:49 +0500

    class changes

--- cypress/e2e/Tounated AddNewPlatform.cy.js
+++ cypress/e2e/Tounated AddNewPlatform.cy.js
@@ -8,7 +8,7 @@ describe("User should create Add New Platform", () => {
     cy.contains('Log In').click()
     cy.wait(8000);
     cy.get(".sidebar-toggler > div").click();
-    cy.get('[style="margin-top: 5px;"] > div').click();
+    cy.get('div[role="button"]').click();
     cy.get(
       '[href="/dashboard/platforms"] > .MuiListItemText-root > .MuiTypography-root'
     ).click();
