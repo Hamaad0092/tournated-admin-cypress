b4126b26c9b6655ad4a83293b48ace474ca3419d
Hamaad0092 <hamaadsaeed9712@gmail.com>
2024-05-14 17:01:48 +0500

    Login today

--- cypress/e2e/Client Side/Login.cy.js
+++ cypress/e2e/Client Side/Login.cy.js
@@ -1,39 +1,35 @@
-const resScreen = [350, 750, 1350, 1800];
 
-resScreen.forEach((value) => {
-describe(`client user Login verification check on screen ${value}`, () => {
-       
-           beforeEach(() => {
-               cy.viewport(value, 1000);
-           });
+describe('client user Login verification check on screen ', () => {
+    beforeEach(() => {
+        cy.visit("https://client.spadasoft.com/")
+    });
 
            it('client should not be login with wrong creds', () => {
-               cy.visit("https://client.spadasoft.com/authentication/login");
+              cy.contains('Login / Register').click()
                cy.get("input[name='email']").type("Test@gmail.com");
                cy.get("input[name='password']").type("123456");
                cy.contains("Log In").click();
            });
 
            it('password should not be a blank space', () => {
-               cy.visit("https://client.spadasoft.com/authentication/login");
+            cy.contains('Login / Register').click()
                cy.get("input[name='email']").type("Test@gmail.com");
                cy.get("input[name='password']").type("     ");
                cy.contains("Log In").click();
            });
 
            it('email should not be a blank', () => {
-               cy.visit("https://client.spadasoft.com/authentication/login");
+            cy.contains('Login / Register').click()
                cy.get("input[name='email']").type("     ");
                cy.get("input[name='password']").type("password");
                cy.contains("Log In").click();
            });
 
            it('Client user should be login with valid creds', () => {
-               cy.visit("https://client.spadasoft.com/authentication/login");
+            cy.contains('Login / Register').click()
                cy.get("input[name='email']").type("superAdmin@gmail.com");
                cy.get("input[name='password']").type("password");
                cy.contains("Log In").click();
            });
        });
-   });
 
