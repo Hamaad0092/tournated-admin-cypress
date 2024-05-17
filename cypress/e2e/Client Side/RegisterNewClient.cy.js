d0dc7271d2e6e87400b491513ddea68ee5a7f44b
Hamaad0092 <hamaadsaeed9712@gmail.com>
2024-05-17 12:46:33 +0500

    registration fix

--- cypress/e2e/Client Side/RegisterNewClient.cy.js
+++ cypress/e2e/Client Side/RegisterNewClient.cy.js
@@ -3,31 +3,50 @@ describe(`Register a new Client `, () => {
     
     it('Register a new client as a athlete ', () => {
             
-            cy.visit("https://client.spadasoft.com/");
-            cy.get('.MuiIconButton-label > .MuiTypography-root').click()
-            cy.contains("Sign up").click()
-            cy.get("input[name='name']").type("Lebron");
-            cy.get("input[name='surname']").type('James');
-            cy.get("input[name='email']").type("Dell666@gmail.com");
-            cy.get("input[name='password']").type('password');
-            cy.get("input[name='reEnter']").type('password');
-            cy.contains("Continue").click()
-            cy.contains("Chess").wait(10000).click()
-            cy.contains("Continue").click()
-            cy.get('input[name="fideId"]').type("12334")
-            cy.get('.PhoneInputInput').clear().type("+923120000000")
-            cy.get('input[name="citizenship"]').clear().type("Pakistan")
-            cy.get('.MuiInputAdornment-root > img').click()
-            cy.get("input[name='dob']").click()
-            cy.get(':nth-child(1) > :nth-child(6) > .MuiButtonBase-root').click()
-            cy.get(':nth-child(2) > .MuiButton-label').click()
-            cy.get('.MuiNativeSelect-select').select('Male')
-            cy.contains("Continue").click()
-            cy.contains("Continue").click()
-            cy.contains("Athlete").click()
-
-            // cy.get("input[name='policy']").click();
-            // cy.contains("Register").click()              ********Button Disabled*******
+        cy.visit("https://client.spadasoft.com/");
+        
+        // Click on the initial button to open the menu
+        cy.get('.MuiIconButton-label > .MuiTypography-root').click();
+        
+        // Click on the Sign Up button
+        cy.contains("Sign up").click();
+        
+        // Fill in the sign-up form
+        cy.get("input[name='name']").type("Test");
+        cy.get("input[name='surname']").type('001');
+        cy.get("input[name='email']").type("test002@gmail.com");
+        cy.get("input[name='password']").type('password');
+        cy.get("input[name='reEnter']").type('password');
+        
+        // Continue to the next step
+        cy.contains("Continue").click();
+        
+        // Select the sport and continue
+        cy.get('input[type="checkbox"]').eq(7).check(); // Ensure the selector is correct for the sport field
+        cy.contains("Continue").click();
+        
+        // Fill in additional details
+        cy.get('input[name="fideId"]').type('12345');
+        cy.get('[name="phoneCountry"]').select("Pakistan").wait(2000)
+        cy.get('input[type="tel"]').type('3120000000');
+        cy.get('input[name="citizenship"]').type("Pakistan").click();
+        
+        // Handle date of birth selection
+        cy.get('.MuiInputAdornment-root > img').click();
+        cy.get("input[name='dob']").click();
+        cy.get(':nth-child(1) > :nth-child(6) > .MuiButtonBase-root').click();
+        cy.get(':nth-child(2) > .MuiButton-label').click();
+        
+        // Select gender and continue
+        cy.get('.MuiNativeSelect-select').select('Male');
+        cy.contains("Continue").click();
+        cy.contains("Continue").click();
+        
+        // Wait for the page to load and ensure that "Federation member" button is visible
+        cy.get('input[type="checkbox"]').eq(1).check();
+        
+        // Click on the Register button and verify if it is enabled
+        // cy.contains("Register").should('not.be.disabled').click();             ********Button Disabled*******
         });
    
 
@@ -75,31 +94,53 @@ describe(`Register a new Client `, () => {
        
     });
 
-    it('if client is an Oranization Memeber', () => {
-       
-          
-            cy.visit("https://client.spadasoft.com/");
-            cy.get('.MuiIconButton-label > .MuiTypography-root').click()
-            cy.contains("Sign up").click()
-            cy.get("input[name='name']").type("Lebron");
-            cy.get("input[name='surname']").type('James');
-            cy.get("input[name='email']").type("Dell666@gmail.com");
-            cy.get("input[name='password']").type('password');
-            cy.get("input[name='reEnter']").type('password');
-            cy.contains("Continue").click()
-            cy.contains("Chess").wait(8000).click()
-            cy.contains("Continue").click()
-            cy.get('.PhoneInputInput').clear().type("+923120000000")
-            cy.get('input[name="citizenship"]').clear().type("Pakistan")
-            cy.get('.MuiInputAdornment-root > img').click()
-            cy.get("input[name='dob']").click()
-            cy.get(':nth-child(1) > :nth-child(6) > .MuiButtonBase-root').click()
-            cy.get(':nth-child(2) > .MuiButton-label').click()
-            cy.get('.MuiNativeSelect-select').select('Male')
-            cy.contains("Continue").click()
-            cy.contains("Continue").click()
-            cy.contains("Federation Member").click()
-            // cy.contains("Register").click()              ********Button Disabled*******
-        });
+    it('if client is an Organization Member', () => {
+        // Visit the client's page
+        cy.visit("https://client.spadasoft.com/");
+        
+        // Click on the initial button to open the menu
+        cy.get('.MuiIconButton-label > .MuiTypography-root').click();
+        
+        // Click on the Sign Up button
+        cy.contains("Sign up").click();
+        
+        // Fill in the sign-up form
+        cy.get("input[name='name']").type("Test");
+        cy.get("input[name='surname']").type('001');
+        cy.get("input[name='email']").type("test002@gmail.com");
+        cy.get("input[name='password']").type('password');
+        cy.get("input[name='reEnter']").type('password');
+        
+        // Continue to the next step
+        cy.contains("Continue").click();
+        
+        // Select the sport and continue
+        cy.get('input[type="checkbox"]').eq(7).check(); // Ensure the selector is correct for the sport field
+        cy.contains("Continue").click();
+        
+        // Fill in additional details
+        cy.get('input[name="fideId"]').type('12345');
+        cy.get('[name="phoneCountry"]').select("Pakistan").wait(2000)
+        cy.get('input[type="tel"]').type('3120000000');
+        cy.get('input[name="citizenship"]').type("Pakistan").click();
+        
+        // Handle date of birth selection
+        cy.get('.MuiInputAdornment-root > img').click();
+        cy.get("input[name='dob']").click();
+        cy.get(':nth-child(1) > :nth-child(6) > .MuiButtonBase-root').click();
+        cy.get(':nth-child(2) > .MuiButton-label').click();
+        
+        // Select gender and continue
+        cy.get('.MuiNativeSelect-select').select('Male');
+        cy.contains("Continue").click();
+        cy.contains("Continue").click();
+        
+        // Wait for the page to load and ensure that "Federation member" button is visible
+        cy.get('input[type="checkbox"]').eq(2).check();
+        
+        // Click on the Register button and verify if it is enabled
+        // cy.contains("Register").should('not.be.disabled').click();
+    });
+    
     });
 
